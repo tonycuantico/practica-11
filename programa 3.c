4db4833d14107f4832c7f33575b2b97256c4808d
tonycuantico <159995704+tonycuantico@users.noreply.github.com>
2024-10-25 14:53:31 -0600

    programa 3.c

---
+++ programa 3.c
@@ -0,0 +1,44 @@
+/* Rdoriguez Arreguin Luis Antonio
+Practica 11 - ejercicio examen*/
+
+#include <stdio.h>
+#define apar = 10
+#define comp = 100
+
+void ini_matrix (int arr[apar][comp])
+{
+    int i, j;
+
+    for( i = 0; i < apar; i++)
+{
+
+
+
+
+void llenar  (arr[10][100])
+{
+  int i, j, num, num2
+  for (i = 0; i < 10; i++)
+{
+   printf("Dame la cantidad: ");
+  scanf ("%d", &num);
+    for(j = 0; j < num; j++)
+{
+  printf("Dame el numero de componente: ");
+  scanf( "%d", &num2);
+  arr[i][num2]=1;
+}
+}
+}
+
+void mensual (apar_men[10])
+{
+  int i ;
+  for( i = 0; i < apar; i++)
+{
+  printf("dame la cantidad mensual del   aparato  %d producido: "; i);
+  scanf("%d",  &apar_men[i]);
+}
+}
+
+
